adb1aaa08ce6e75e3d91ae88de51f89633407b32
Dev Saini <137302375+DEVKUMARSAINI545@users.noreply.github.com>
2025-08-24 16:50:07 +0530

    Update and rename dockercompose.groovy to build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def run(String dockerhubuser){
+def call(String dockerhubuser){
   withCredentials([usernamePassword(credentialsId:'dockerhubCred', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]){ 
   sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}
   }
