8b98a976d2dce86746b0c7f1f84c10c2da7c9879
Dev Saini <137302375+DEVKUMARSAINI545@users.noreply.github.com>
2025-08-24 16:52:23 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 def call(String dockerhubuser){
   withCredentials([usernamePassword(credentialsId:'dockerhubCred', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]){ 
-  sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}
+  sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
   sh "docker compose up -d "
 }
